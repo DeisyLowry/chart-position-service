0cd2ba4bdb33f82f42473e81aca2c2cd9046903c
David Lowry <DavidLowry64@gmail.com>
2023-07-25 15:43:32 -0400

    Initial commit

---
+++ src/main/java/com/ttrc/chartposition/ChartPositionServiceApplication.java
@@ -0,0 +1,14 @@
+package com.ttrc.chartposition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartPositionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartPositionServiceApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/ttrc/chartposition/ChartPositionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttrc.chartposition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartPositionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
